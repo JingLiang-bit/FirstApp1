952cc063dc0356c66d19e708efdd1ff0dbfc9b0d
JingLiang <2270176818@qq.com>
2020-04-02 16:21:35 +0800

    跳转页面并返回+菜单

---
+++ app/src/main/java/com/example/firstapp/ConfigActivity.java
@@ -0,0 +1,63 @@
+package com.example.firstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfigActivity extends AppCompatActivity {
+
+    public final String TAG="ConfigActivity";
+
+    EditText dollarText;
+    EditText euroText;
+    EditText wonText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+        //在被传递参数的页面，用getIntent获得传递过来的参数；
+        Intent intent =getIntent();
+        //获取数据的时候是有类型的；
+        float dollar2=intent.getFloatExtra("dollar_rate_key",0.0f);
+        float euro2=intent.getFloatExtra("euro_rate_key",0.0f);
+        float won2=intent.getFloatExtra("euro_rate_key",0.0f);
+
+        Log.i(TAG,"onCreate:dollar2="+dollar2);
+        Log.i(TAG,"onCreate:euro2="+euro2);
+        Log.i(TAG,"onCreate:won2="+won2);
+
+        dollarText=(EditText)findViewById(R.id.dollar_rate);
+        euroText=(EditText)findViewById(R.id.euro_rate);
+        wonText=(EditText)findViewById(R.id.won_rate);
+
+        //显示数据到控件；
+        dollarText.setText(String.valueOf(dollar2));
+        euroText.setText(String.valueOf(euro2));
+        wonText.setText(String.valueOf(won2));
+    }
+
+    public void save(View btn){
+        //获取新的数据；
+        float newDollar=Float.parseFloat(dollarText.getText().toString());
+        float newEuro=Float.parseFloat(euroText.getText().toString());
+        float newWon=Float.parseFloat(wonText.getText().toString());
+
+        //保存到Bundle或放入到Extra;
+        Intent intent=getIntent();
+        Bundle bdl=new Bundle();
+        bdl.putFloat("dollar_new_key",newDollar);
+        bdl.putFloat("euro_new_key",newEuro);
+        bdl.putFloat("won_new_key",newWon);
+        intent.putExtras(bdl);
+        setResult(2,intent);  //返回编码；
+
+        //返回到调用页面；
+        finish();
+    }
+}
--- app/src/main/java/com/example/firstapp/RateActivity.java
+++ app/src/main/java/com/example/firstapp/RateActivity.java
@@ -1,16 +1,25 @@
 package com.example.firstapp;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RateActivity extends AppCompatActivity {
+
+    private final String TAG="Rate";
+    private float dollarRate=0.1f;
+    private float euroRate=0.1f;
+    private float wonRate=0.1f;
+
 
     EditText rmb;
     TextView showOut;
@@ -29,23 +38,67 @@ public class RateActivity extends AppCompatActivity {
         float r = 0;
         if (str.length() > 0) {
             r = Float.parseFloat(str);
-        } else {
+        }
+        else {
             Toast.makeText(this, "请输入金额", Toast.LENGTH_SHORT).show();
         }
-        float val;
+
         if (btn.getId() == R.id.btn_dollar) {
-            val = r * (1 / 7.08f);
+            showOut.setText(String.format("%.2f",r*dollarRate));
         } else if (btn.getId() == R.id.btn_euro) {
-            val = r * (1 / 7.75f);
+            showOut.setText(String.format("%.2f",r*euroRate));
         } else {
-            val = r * 178.0f;
+            showOut.setText(String.format("%.2f",r*wonRate));
         }
-        showOut.setText(String.valueOf(val));
     }
 
     public void openOne(View btn){
-        //Intent hello=new Intent(this,SecondActivity.class);
-        Intent web=new Intent(Intent.ACTION_VIEW, Uri.parse("https://authserver.swufe.edu.cn"));
-        startActivity(web);
+        openConfig();
+    }
+
+    private void openConfig() {
+        //跳转网页
+        //Intent web=new Intent(Intent.ACTION_VIEW, Uri.parse("https://authserver.swufe.edu.cn"));
+
+
+        //把参数传递到下一个页面，用Intent方法传递参数
+        Intent config = new Intent(this, ConfigActivity.class);
+        config.putExtra("dollar_rate_key", dollarRate);
+        config.putExtra("euro_rate_key", euroRate);
+        config.putExtra("won_rate_key", wonRate);
+
+        //这样的方法打开，只能不能返回，只能无限打开新的界面
+        // startActivity(config);
+
+        startActivityForResult(config, 1);//可以打开并带回数据；1是请求参数；
+    }
+
+    //处理Config页面带回来的数据
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //因为一个窗口可以打开多个窗口，返回参数区别哪个窗口返回的数据；
+        //resultCode可能带回不同的数据，我们区分返回的是怎样的数据；
+        if(resultCode==1&&resultCode==2){
+            Bundle bundle=data.getExtras();
+            dollarRate=bundle.getFloat("dollar_new_key",0.1f);//这里的key应该是更改后的；
+            euroRate=bundle.getFloat("euro_new_key",0.2f);
+            wonRate=bundle.getFloat("won_new_key",0.3f);
+        }
+        super.onActivityResult(requestCode,resultCode,data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.rate,menu);//Menu填充器；
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.menu_set){
+            openConfig();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
